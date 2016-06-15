6f752ac98bbe0685ec6e28da6bee5daca8681206
Vadus <vadus2@gmx.de>
2016-06-16 00:21:46 +0200

    initial version with unit test
    see http://codekata.com/kata/kata09-back-to-the-checkout/

---
+++ src/main/java/org/klarblick/kata/checkout/CheckOut.java
@@ -0,0 +1,17 @@
+package org.klarblick.kata.checkout;
+
+import java.util.List;
+
+public class CheckOut {
+
+	public CheckOut(List<PricingRule> pricingRules) {
+	}
+	
+	public void scan(String sku){
+		
+	}
+	
+	public int total(){
+		return -1;
+	}
+}
---
+++ src/main/java/org/klarblick/kata/checkout/PricingRule.java
@@ -0,0 +1,5 @@
+package org.klarblick.kata.checkout;
+
+public class PricingRule {
+
+}
